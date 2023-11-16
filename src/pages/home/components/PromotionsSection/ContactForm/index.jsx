0a6c4440f3ca40dcb0f423b8f9dbe25d802962d3
ailyfdez <ailyfdez89@gmail.com>
2023-11-16 17:13:45 -0300

    validate contact form

--- src/pages/home/components/PromotionsSection/ContactForm/index.jsx
+++ src/pages/home/components/PromotionsSection/ContactForm/index.jsx
@@ -1,10 +1,12 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Button, Typography, TextField, Box } from "@mui/material";
 import ReCAPTCHA from "react-google-recaptcha";
 
 const ContactForm = ({ isDesktop }) => {
   const [contact, setContact] = React.useState(null);
   const [captcha, setCaptcha] = React.useState(null);
+  const [isValid, setIsValid] = React.useState(false);
+
   const recaptchaRef = useRef();
 
   const onChangeValue = (value, prop) => {
@@ -16,15 +18,33 @@ const ContactForm = ({ isDesktop }) => {
     setCaptcha(value);
   };
 
+  const onSubmit = (e, contact, captcha) => {
+    e.preventDefault();
+    recaptchaRef.current.reset();
+    setContact({});
+    setCaptcha(null);
+  };
+
+  useEffect(() => {
+    if (contact?.name && contact?.email && contact?.message && captcha) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [contact, captcha]);
+
   return (
     <>
       <Box
         component="form"
+        onSubmit={(e) => {
+          onSubmit(e, contact, captcha, recaptchaRef);
+        }}
         sx={{
           zIndex: 1000,
           "& .MuiTextField-root": { m: 1 },
           "& .MuiInputLabel-standard": { color: "#FFFFFF" },
-          backgroundColor: "rgba(7, 83, 145, 0.4)",
+          backgroundColor: "rgba(1, 1, 1, 0.8)",
           margin: isDesktop ? "140px 0 100px 12vw" : "100px 0 50px 0",
           display: "grid",
           gridTemplateColumns: { sm: "1fr" },
@@ -40,17 +60,16 @@ const ContactForm = ({ isDesktop }) => {
           placeholder="Enter your full name"
           color="secondary"
           variant="standard"
-          value={contact?.name}
+          value={contact?.name ?? ""}
           name="name"
           onChange={(e) => onChangeValue(e.target.value, e.target.name)}
         />
         <TextField
-          required
           label={contact?.phone ? "Phone number" : ""}
           placeholder="Enter your phone number"
           color="secondary"
           variant="standard"
-          value={contact?.phone}
+          value={contact?.phone ?? ""}
           name="phone"
           onChange={(e) => onChangeValue(e.target.value, e.target.name)}
         />
@@ -60,17 +79,16 @@ const ContactForm = ({ isDesktop }) => {
           placeholder="Enter your email"
           color="secondary"
           variant="standard"
-          value={contact?.email}
+          value={contact?.email ?? ""}
           name="email"
           onChange={(e) => onChangeValue(e.target.value, e.target.name)}
         />
         <TextField
-          required
           label={contact?.address ? "Address" : ""}
           placeholder="Enter your address"
           color="secondary"
           variant="standard"
-          value={contact?.address}
+          value={contact?.address ?? ""}
           name="address"
           onChange={(e) => onChangeValue(e.target.value, e.target.name)}
         />
@@ -82,18 +100,18 @@ const ContactForm = ({ isDesktop }) => {
           variant="standard"
           multiline
           rows={3}
-          value={contact?.message}
+          value={contact?.message ?? ""}
           name="message"
           onChange={(e) => onChangeValue(e.target.value, e.target.name)}
         />
-       
+
         <ReCAPTCHA
           ref={recaptchaRef}
           sitekey="6LcB-ysUAAAAAE_uDz0N0IiwjdwFGbqUTfcFi_Ey"
           onChange={onChangeCaptcha}
           theme="dark"
         />
-        <Button variant="contained">
+        <Button variant="contained" type="submit" disabled={!isValid}>
           <Typography variant="button" color="primary.light">
             GET A FREE QUOTE
           </Typography>
