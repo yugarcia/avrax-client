279a2a1cf94e8ad9b0dcefb07760b8218b7ade31
ailyfdez <ailyfdez89@gmail.com>
2023-11-23 00:06:46 -0300

    fix percentage calc

--- src/components/RegresiveCount/index.jsx
+++ src/components/RegresiveCount/index.jsx
@@ -5,7 +5,7 @@ import CircularTime from "./CircularTime.jsx";
 
 const DATE = "2023-12-29T00:00:00.000Z";
 
-const RegresiveCount = ({isMobile}) => {
+const RegresiveCount = ({ isMobile }) => {
   const currentDate = new Date();
   const date = new Date(DATE);
   const diff = date - currentDate;
@@ -13,8 +13,8 @@ const RegresiveCount = ({isMobile}) => {
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
 
-  const hoursPercentage = (hours * 100) / 24;
-  const minutesPercentage = (minutes * 100) / 60;
+  const hoursPercentage = 100 - (hours * 100) / 24;
+  const minutesPercentage = 100 - (minutes * 100) / 60;
 
   return (
     <Container isMobile={isMobile}>
