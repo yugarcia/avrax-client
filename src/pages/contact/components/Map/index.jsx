97f783cd02e23cb0f2ecdabc5a79a2f457727bd1
ailyfdez <ailyfdez89@gmail.com>
2023-11-29 22:38:11 -0300

    revert las commit

--- src/pages/contact/components/Map/index.jsx
+++ src/pages/contact/components/Map/index.jsx
@@ -6,8 +6,7 @@ import React, { useState, useEffect } from "react";
 const ADDRESS = "8350 NW 52nd Terrace, Doral, FL 33166, EE. UU"; // Dirección seleccionada
 
 const Map = () => {
-  const apiKey =
-    "CI22yQEIo7bJAQipncoBCJ7sygEIkqHLAQiGoM0BCOnFzQEIusjNARjAy8wBGKfqzQE="; // Reemplaza con tu clave de API de Google Maps
+  const apiKey = "CI22yQEIo7bJAQipncoBCJ7sygEIkqHLAQiGoM0BCOnFzQEIusjNARjAy8wBGKfqzQE="; // Reemplaza con tu clave de API de Google Maps
 
   const containerStyle = {
     width: "100%",
@@ -16,40 +15,39 @@ const Map = () => {
 
   const [coordenadas, setCoordenadas] = useState({ lat: 0, lng: 0 });
 
-  // useEffect(() => {
-  //   const obtenerCoordenadas = async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-  //           ADDRESS
-  //         )}&key=${apiKey}`
-  //       );
+  useEffect(() => {
+    const obtenerCoordenadas = async () => {
+      try {
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+            ADDRESS
+          )}&key=${apiKey}`
+        );
 
-  //       const resultado = response.data.results[0];
-  //       if (resultado) {
-  //         const { lat, lng } = resultado.geometry.location;
-  //         setCoordenadas({ lat, lng });
-  //       }
-  //     } catch (error) {
-  //       console.error("Error al obtener las coordenadas:", error);
-  //     }
-  //   };
+        const resultado = response.data.results[0];
+        if (resultado) {
+          const { lat, lng } = resultado.geometry.location;
+          setCoordenadas({ lat, lng });
+        }
+      } catch (error) {
+        console.error("Error al obtener las coordenadas:", error);
+      }
+    };
 
-  //   obtenerCoordenadas();
-  // }, [ADDRESS, apiKey]);
+    obtenerCoordenadas();
+  }, [ADDRESS, apiKey]);
 
   return (
-    <>Mapa</>
-    // <LoadScript googleMapsApiKey={apiKey}>
-    //   <GoogleMap
-    //     mapContainerStyle={containerStyle}
-    //     center={coordenadas}
-    //     zoom={15}
-    //   >
-    //     {/* Marcador en el centro del mapa */}
-    //     <Marker position={coordenadas} />
-    //   </GoogleMap>
-    // </LoadScript>
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={coordenadas}
+        zoom={15}
+      >
+        {/* Marcador en el centro del mapa */}
+        <Marker position={coordenadas} />
+      </GoogleMap>
+    </LoadScript>
   );
 };
 
