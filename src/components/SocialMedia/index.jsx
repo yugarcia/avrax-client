ca63ee8afff0e001777aa28589a2777031dbd272
ailyfdez <ailyfdez89@gmail.com>
2024-02-07 12:22:39 -0300

    fix twitter links

--- src/components/SocialMedia/index.jsx
+++ src/components/SocialMedia/index.jsx
@@ -6,7 +6,7 @@ const SocialMedia = () => {
   return (
     <SocialMediaContainer>
       <Icon icon="facebook" link="https://m.facebook.com/avraxwindows" />
-      <Icon icon="twitter" link="https://x.com//CesarEs59421082" />
+      <Icon icon="twitter" link="https://x.com/CesarEs59421082" />
       <Icon icon="instagram" link="https://www.instagram.com/avrax.windows" />
       <Icon icon="youtube" link="https://www.youtube.com/@AvraxImpactWindows" />
     </SocialMediaContainer>
